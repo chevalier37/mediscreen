9cfdef34ad27b2cea0a7ed819e64980df260cd8c
Jean-Benoit <jean.benoit.roussat@gmail.com>
2020-08-06 16:27:19 +0200

    Sprint 1 50%

--- Mirco_service_patient/src/main/java/com/mediscreen/controller/PatientController.java
+++ Mirco_service_patient/src/main/java/com/mediscreen/controller/PatientController.java
@@ -13,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.mediscreen.model.Patient;
@@ -22,11 +23,17 @@ import com.mediscreen.service.PatientService;
 public class PatientController {
 
 	@Autowired
-	PatientService PatientService;
+	PatientService patientService;
 
-	@GetMapping("addPatient")
+	@GetMapping("patient/add")
 	public String addPatient(Patient patient) {
 		return "Patient/add";
+	}
+
+	@GetMapping("/patient/list")
+	public String listPatient(Model model) {
+		model.addAttribute("allPatient", patientService.listPatient());
+		return "Patient/list";
 	}
 
 	@PostMapping("/patient/validate")
@@ -38,26 +45,52 @@ public class PatientController {
 		if (!result.getFieldValue("dateOfBirth").toString().isEmpty()) {
 			birth = simpleDateFormat.parse(result.getFieldValue("dateOfBirth").toString());
 		}
+
 		String firstName = result.getFieldValue("firstName").toString();
-
 		String familyName = result.getFieldValue("familyName").toString();
-
 		String gender = result.getFieldValue("gender").toString();
-
 		String address = result.getFieldValue("address").toString();
-
 		String phone = result.getFieldValue("phone").toString();
 
 		Patient newPatient = new Patient(firstName, familyName, birth, gender, address, phone);
 
 		if (!result.hasErrors()) {
-			PatientService.addPatient(newPatient);
-			// model.addAttribute("allBidList", bidListService.findAllBidList());
-			// return "redirect:/bidList/list";
+			patientService.addPatient(newPatient);
+			model.addAttribute("allPatient", patientService.listPatient());
+			return "redirect:/patient/list";
 		}
 
 		return "Patient/add";
 
+	}
+
+	@GetMapping("/patient/update/{id}")
+	public String showUpdateForm(@PathVariable("id") String id, Model model) {
+		Patient patient = patientService.getPatient(id)
+				.orElseThrow(() -> new IllegalArgumentException("Invalid patient Id:" + id));
+
+		model.addAttribute("patient", patient);
+		return "Patient/update";
+	}
+
+	@PostMapping("/patient/update/{id}")
+	public String updateBid(@PathVariable("id") String id, @Valid Patient patient, BindingResult result, Model model) {
+		if (!result.hasErrors()) {
+			patientService.addPatient(patient);
+			model.addAttribute("allPatient", patientService.listPatient());
+			return "redirect:/patient/list";
+		}
+
+		return "redirect:/patient/list";
+	}
+
+	@GetMapping("/patient/{id}")
+	public String showPatient(@PathVariable("id") String id, Model model) {
+		Patient patient = patientService.getPatient(id)
+				.orElseThrow(() -> new IllegalArgumentException("Invalid patient Id:" + id));
+
+		model.addAttribute("patient", patient);
+		return "Patient/showPatient";
 	}
 
 }
--- Mirco_service_patient/src/main/java/com/mediscreen/service/PatientService.java
+++ Mirco_service_patient/src/main/java/com/mediscreen/service/PatientService.java
@@ -1,5 +1,8 @@
 package com.mediscreen.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,14 @@ public class PatientService {
 
 	public Patient addPatient(Patient patient) {
 		return PatientRepository.save(patient);
+	}
+
+	public List<Patient> listPatient() {
+		return PatientRepository.findAll();
+	}
+
+	public Optional<Patient> getPatient(String id) {
+		return PatientRepository.findById(id);
 	}
 
 }
