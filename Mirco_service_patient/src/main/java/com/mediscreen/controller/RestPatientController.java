f2ddc5dc9f5ada56b6fa22d35342152c4321fc5d
Jean-Benoit <jean.benoit.roussat@gmail.com>
2020-08-25 10:01:16 +0200

    Projet terminé

--- Mirco_service_patient/src/main/java/com/mediscreen/controller/RestPatientController.java
+++ Mirco_service_patient/src/main/java/com/mediscreen/controller/RestPatientController.java
@@ -3,6 +3,9 @@ package com.mediscreen.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +23,19 @@ public class RestPatientController {
 	public Patient showPatient(@PathVariable("id") int id) {
 		return patientService.getPatientById(id)
 				.orElseThrow(() -> new IllegalArgumentException("Invalid patient Id:" + id));
+	}
+
+	@PostMapping("/addpatient")
+	public Patient validate(@RequestBody Patient patient) {
+		patientService.addPatient(patient);
+		return patient;
+
+	}
+
+	@PutMapping("/updatePatient")
+	public Patient updatePatient(@RequestBody Patient patient) {
+		patientService.addPatient(patient);
+		return patient;
 	}
 
 }
